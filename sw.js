b5b1c680dfc3e6aefa6825e9b1d1470d83b4eac5
n9o3oo9-bit <n9o3oo9@gmail.com>
2026-08-04 14:20:51 +0300

    Update icon paths in service worker

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 const CACHE = 'masareefi-v3';
 const SHELL = [
   './', './index.html', './manifest.webmanifest', './icon.svg',
-  './icons/icon-192.png', './icons/icon-512.png',
-  './icons/icon-maskable-512.png', './icons/apple-touch-icon.png',
+  './icon-192.png', './icon-512.png',
+  './icon-maskable-512.png', './apple-touch-icon.png',
 ];
 
 self.addEventListener('install', e => {
